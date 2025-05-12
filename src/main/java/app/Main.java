b10e278887d146648ace22fdb9bd18264e02eeea
AlexMules <alexandru.mules@gmail.com>
2025-05-12 20:49:18 +0300

    Added JTable

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,20 +1,8 @@
-package org.example;
+package app;
 
-import access.ClientDAO;
-import access.OrderDAO;
-import access.ProductDAO;
 import gui.view.MainView;
-import logic.ClientBLL;
-import logic.OrderBLL;
-import logic.ProductBLL;
-import model.Client;
-import model.Order;
-import model.Product;
 
 import javax.swing.*;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
