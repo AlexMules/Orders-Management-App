a82b026cbeaaf3d9b4affe38717c24d97cbcc5a7
AlexMules <alexandru.mules@gmail.com>
2025-05-16 09:16:40 +0300

    Added GUI for viewing bills

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,12 +1,9 @@
 package app;
 
 import gui.view.MainView;
-
 import javax.swing.*;
-import java.util.logging.Logger;
 
 public class Main {
-    protected static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
 
